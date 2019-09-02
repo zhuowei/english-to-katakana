26d2828c8f01193da330f9b809912d86085e5224
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-09-02 16:23:37 -0400

    Update crx build

--- make_crx.sh
+++ make_crx.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
 python3 katapult/import_words.py Source/template.js katapult/en_to_jp.json Source/content_script.js
-cd Source
-7z a ../EnglishToKatakana.zip content_script.js manifest.json
-cd ..
-mv EnglishToKatakana.zip EnglishToKatakana.crx
+node node_modules/crx/src/cli.js pack Source EnglishToKatakana.crx
