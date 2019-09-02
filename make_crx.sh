d62039975091569fcaf9ce57094f7b3ef1d13ded
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-09-02 16:37:17 -0400

    try again

--- make_crx.sh
+++ make_crx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 python3 katapult/import_words.py Source/template.js katapult/en_to_jp.json Source/content_script.js
-node node_modules/crx/src/cli.js pack Source EnglishToKatakana.crx
+node node_modules/crx/src/cli.js pack Source -o EnglishToKatakana.crx
