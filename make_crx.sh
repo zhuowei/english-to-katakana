d76a57c7ba3202b153eeda444a1a313bd4bc959f
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-09-02 16:03:54 -0400

    update manifest

--- make_crx.sh
+++ make_crx.sh
@@ -2,6 +2,6 @@
 set -e
 python3 katapult/import_words.py Source/template.js katapult/en_to_jp.json Source/content_script.js
 cd Source
-7z a ../EnglishToKatakana.zip .
+7z a ../EnglishToKatakana.zip content_script.js manifest.json
 cd ..
 mv EnglishToKatakana.zip EnglishToKatakana.crx
